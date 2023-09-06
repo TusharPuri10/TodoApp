690508e574f15bf4f9e8da898dea5f21e0c04b45
Tushar Puri <92613357+TusharPuri10@users.noreply.github.com>
2023-09-07 04:45:01 +0530

    final UI DONE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,60 +1,62 @@
-import Signup from "./components/Signup"
-import Appbar from "./components/Appbar"
-import TodoPage from "./components/TodoPage"
-import './App.css'
-import {BrowserRouter, Routes, Route, NavLink} from 'react-router-dom'
-import Login from "./components/Login"
-import { useState,forwardRef,useEffect } from "react";
-import { Grow } from "@mui/material"
-import { TodoUpdate } from "./components/TodoUpdate"
+import Signup from "./components/Signup";
+import Appbar from "./components/Appbar";
+import TodoPage from "./components/TodoPage";
+import "./App.css";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Login from "./components/Login";
+import { useState, forwardRef, useEffect } from "react";
+import { Grow } from "@mui/material";
+import { TodoUpdate } from "./components/TodoUpdate";
 import { isShownState } from "./states/Todos";
 import { useRecoilState } from "recoil";
 import Home from "./components/Home";
+import OutsideClickHandler from "react-outside-click-handler";
 
-function App()
-{
+function App() {
   const [isCardShown, setCardShown] = useRecoilState(isShownState);
 
   const UpdateCard = forwardRef(function (props, ref) {
-
-    if(isCardShown!=null)
-    {
+    if (isCardShown != null) {
       return (
         <div ref={ref} {...props}>
-          <TodoUpdate/>
+          <OutsideClickHandler
+            onOutsideClick={() => {
+              setCardShown(null);
+            }}
+          >
+            <TodoUpdate />
+          </OutsideClickHandler>
         </div>
       );
-    }
-    else
-    {
-      return <div ref={ref} {...props}>
-      </div>
+    } else {
+      return <div ref={ref} {...props}></div>;
     }
   });
 
-
-  return <div>
+  return (
+    <div>
       <div className={isCardShown ? "overlay" : "underlay"}>
-        <Grow in={isCardShown?true:false}>
+        <Grow in={isCardShown ? true : false}>
           <div>
-              <UpdateCard/>
+            <UpdateCard />
           </div>
         </Grow>
       </div>
       <div className={isCardShown ? "app-main blur-page" : "app-main"}>
         <BrowserRouter basename="/">
-        <Appbar/>
-        <div className="scroll">
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/> 
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/todos" element={<TodoPage/>}/>
-          </Routes>
-        </div>
+          <Appbar />
+          <div className="scroll">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/todos" element={<TodoPage />} />
+            </Routes>
+          </div>
         </BrowserRouter>
       </div>
-  </div>
+    </div>
+  );
 }
 
 export default App;
